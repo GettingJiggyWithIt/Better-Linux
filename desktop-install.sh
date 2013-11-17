f9319559b8438bc117c4ccb9e5e4a1dcf1f6d48d
GigabyteIO <brian@enviedsolutions.com>
2013-11-16 20:16:50 -0500

    Create desktop-install.sh

---
+++ desktop-install.sh
@@ -0,0 +1,39 @@
+# Source: http://www.webupd8.org/2013/10/8-things-to-do-after-installing-ubuntu.html
+
+# Installs My Weather Indicator
+sudo add-apt-repository ppa:atareao/atareao
+sudo apt-get update
+sudo apt-get install my-weather-indicator
+
+# Installs System Load Indicator
+sudo apt-get install indicator-multiload
+
+# Installs Diodon Clipboard Manger
+sudo apt-get install diodon diodon-plugins
+
+# Installs Ubuntu Tweak Tool
+sudo apt-get install unity-tweak-tool
+
+# Installs codecs, Java, and encrypted DVD playback
+sudo apt-get install ubuntu-restricted-extras
+
+# Installs encrypted DVD support
+sudo apt-get install libdvdread4
+sudo /usr/share/doc/libdvdread4/install-css.sh
+
+# Installs unrestricted codecs for video editors/transcoders
+sudo apt-get install libavformat-extra-53 libavcodec-extra-53
+
+# Installs Java User support
+sudo apt-get install icedtea-7-plugin openjdk-7-jre
+
+# Installs Java developement support
+sudo apt-get install openjdk-7-jdk
+
+# Installs Laptop mode tools
+sudo apt-get install laptop-mode-tools
+
+# Installs latest VLC player
+sudo add-apt-repository ppa:ed10vi86/video
+sudo apt-get update
+sudo apt-get install vlc
