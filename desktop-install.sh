4a1cc4e7eed306007fc895f3997057078af8d184
GigabyteIO <brian@enviedsolutions.com>
2013-11-17 03:08:32 -0500

    Update desktop-install.sh

--- desktop-install.sh
+++ desktop-install.sh
@@ -1,12 +1,11 @@
-# Source: http://www.webupd8.org/2013/10/8-things-to-do-after-installing-ubuntu.html
-
-# Installs My Weather Indicator
-sudo add-apt-repository ppa:atareao/atareao
+# Update system
 sudo apt-get update
-sudo apt-get install my-weather-indicator
 
 # Installs System Load Indicator
 sudo apt-get install indicator-multiload
+
+# Installs Chromium - developer version of Chrome (open source)
+sudo apt-get install chromium-browser 
 
 # Installs Diodon Clipboard Manger
 sudo apt-get install diodon diodon-plugins
@@ -30,12 +29,9 @@ sudo apt-get install icedtea-7-plugin openjdk-7-jre
 # Installs Java developement support
 sudo apt-get install openjdk-7-jdk
 
-# Installs latest VLC player
-sudo add-apt-repository ppa:ed10vi86/video
-sudo apt-get update
+# Installs VLC player
 sudo apt-get install vlc
 
-# Source: http://itsfoss.com/things-to-do-after-installing-ubuntu-13-10/
 # Installs flash plugin
 sudo apt-get install flashplugin-installer
 
@@ -48,23 +44,23 @@ sudo add-apt-repository ppa:linrunner/tlp
 sudo apt-get update
 sudo apt-get install tlp tlp-rdw
 sudo tlp start
-sudo apt-get install chromium-browser 
-sudo apt-get install unity-tweak-tool 
+
+# Installs Skype
 sudo apt-get install skype
-sudo add-apt-repository ppa:me-davidsansome/clementine
-sudo apt-get update
-sudo apt-get install clementine
-sudo add-apt-repository ppa:cooperjona/nitrotasks
-sudo apt-get update
+
+# Installs Nitrotasks
 sudo apt-get install nitrotasks
+
 # Installs extra codecs form multimedia users
 sudo apt-get install gstreamer0.10-plugins-ugly gstreamer0.10-ffmpeg libxine1-ffmpeg gxine mencoder libdvdread4 totem-mozilla icedax tagtool easytag id3tool lame nautilus-script-audio-convert libmad0 mpg321
 
 # Installs MIXXX
-sudo add-apt-repository ppa:mixxx/mixxx
-sudo apt-get update
 sudo apt-get install mixxx libportaudio2
 
+# Installs Filezilla
+sudo apt-get install filezilla
+
+# Installs Calendar Indicator
 sudo add-apt-repository ppa:noobslab/indicators
 sudo apt-get update
 sudo apt-get install calendar-indicator
@@ -72,9 +68,7 @@ sudo apt-get install calendar-indicator
 gparted dropbox nitro
 free license for Bitdefender
 http://www.bitdefender.com/site/Products/ScannerLicense/
-sudo apt-get install filezilla
-sudo add-apt-repository ppa:numix/ppa
 
-sudo apt-get update
+sudo add-apt-repository ppa:numix/ppa
 
 sudo apt-get install numix-gtk-theme numix-icon-theme numix-wallpaper-saucy
