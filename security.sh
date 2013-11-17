5a0ca241d1a804fa7938a394ef27ba733c5b7409
GigabyteIO <brian@enviedsolutions.com>
2013-11-16 21:29:27 -0500

    Create security.sh

---
+++ security.sh
@@ -0,0 +1,7 @@
+# Source: http://www.vogella.com/articles/Ubuntu/article.html
+sudo apt-get install ufw
+sudo ufw allow 80/tcp # HTTP
+sudo ufw allow 22/tcp # SSH
+sudo ufw allow 443/tcp # HTTPS
+sudo ufw allow 21/tcp # FTP
+sudo ufw enable
