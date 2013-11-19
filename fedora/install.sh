9aee270705257df9996d52f837a93829aaa31dae
GettingJiggyWithIt <brian@enviedsolutions.com>
2013-11-18 23:50:21 -0500

    Update install.sh

--- fedora/install.sh
+++ fedora/install.sh
@@ -5,3 +5,8 @@ su -c 'yum localinstall --nogpgcheck http://download1.rpmfusion.org/free/fedora/
 
 # Add Fedora Utils
 su -c "curl http://master.dl.sourceforge.net/project/fedorautils/fedorautils.repo -o /etc/yum.repos.d/fedorautils.repo && yum install fedorautils"
+
+# Requirement for theme
+# Theme:http://gnome-look.org/content/show.php/Zukitwo?content=140562
+sudo yum install gtk-murrine-engine gtk2-engines
+sudo yum install gnome-tweak-tool
