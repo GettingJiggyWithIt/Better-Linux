ab7d3b12592ca24aa9a090ea4e0e028d3cb31b92
Brian Zalewski <MByteIO@users.noreply.github.com>
2014-03-28 14:56:46 -0400

    Update install.sh

--- fedora/install.sh
+++ fedora/install.sh
@@ -4,10 +4,10 @@
 su -c 'yum localinstall --nogpgcheck http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm'
 
 # Add Fedora Utils
-su -c "curl http://master.dl.sourceforge.net/project/fedorautils/fedorautils.repo -o /etc/yum.repos.d/fedorautils.repo && yum install fedorautils"
+su -c "curl http://satya164.github.io/fedy/fedy-installer -o fedy-installer && chmod +x fedy-installer && ./fedy-installer"
 
 # Requirement for theme
 # Theme:http://gnome-look.org/content/show.php/Zukitwo?content=140562
-sudo yum install gtk-murrine-engine gtk2-engines
-sudo yum install gnome-tweak-tool
+# sudo yum install gtk-murrine-engine gtk2-engines
+# sudo yum install gnome-tweak-tool
 # sudo unzip /home/$username/Downloads/$themefilename -d /usr/share/themes # Adds theme for all users
